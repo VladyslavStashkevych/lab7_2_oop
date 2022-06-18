334ead3305a2f152a592dad798b2cdfd46fdbf69
VladyslavStashkevych <vladyslav.stashkevych.it.2021@lpnu.ua>
2022-06-18 21:18:48 +0300

    Add project files.

---
+++ lab7_2/Source.cpp
@@ -0,0 +1,42 @@
+// Source.cpp
+#include <iostream>
+#include <vector>
+#include <ctime>
+using namespace std;
+void create(vector<int>& arr, int n);
+void print(vector<int> arr, int n);
+bool task(vector<int> arr, int n);
+
+int main() {
+	srand((unsigned int)time(NULL));
+	int n;
+	cout << "n = ? "; cin >> n;
+	vector<int> arr = { 0 };
+	create(arr, n);
+	print(arr, n);
+	cout << "AVG = " << task(arr, n) << endl;
+
+	system("pause");
+	return 0;
+}
+
+void create(vector<int>& arr, int n)
+{
+	for (int i = 0; i < n; i++)
+		arr[i] = rand() % 100;
+}
+void print(vector<int> arr, int n)
+{
+	for (int i = 0; i < n; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+bool task(vector<int> arr, int n) {
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+		if (arr[i] % 2 == 0){
+			sum += arr[i];
+		}
+	return sum;
+}
